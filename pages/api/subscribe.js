f523aa949617b59c2ab59ed8ce3dd8dda4cbb68d
Rishabh Garg <rishabhgarg@Rishabhs-MacBook-Air.local>
2020-12-10 11:00:40 +0530

    changeds

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -3,24 +3,26 @@ export default async(req, res) => {
   const { email, tags } = req.query;
 
   if (!email) {
-     return res.status(400).json({ error: "Email is required" }).end() ;
+     return res.status(400).json({ error: "Email is required" }) ;
   }
 
   try {
-
-    const response = await fetch(`${process.env.CONVERTKIT_API_URL}/forms/${process.env.CONVERTKIT_FORM_ID}/subscribe`, {
+    console.log(process.env.CONVERTKIT_API_URL)
+    const endpoint = `${process.env.CONVERTKIT_API_URL}/forms/${process.env.CONVERTKIT_FORM_ID}/subscribe`
+    console.log({endpoint})
+    const response = await fetch(endpoint, {
       method: 'POST',
       headers: {'Content-Type': 'application/json; charset=utf-8'},
       body: JSON.stringify({"api_key": process.env.CONVERTKIT_API_KEY, "email": email, "tags": tags })});
   
     if (response.status >=400){
-      return res.status(400).json({error:"Something went wrong while subscribing to API"}).end()
+      return res.status(400).json({error:"Something went wrong while subscribing to API"})
     }
   
     res.status(201).json({'message':'Subscribed.'})
 
   } catch(error){
-    return res.status(400).json({error:"Something went wrong."}).end()
+    return res.status(400).json({error:`Something went wrong. Error: ${error}`})
   }
 
 
